d1f5e7ecfe8f3e14200b343406f02ac3228522fe
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-15 17:33:40 -0400

    MVP complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,11 +1,32 @@
 // Test away!
 // Test away
 import React from "react";
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, getByTestId } from "react-testing-library";
 import Controls from "./Controls";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
 
 describe("Controls", () => {
   it("should render without failing", () => {
     render(<Controls />);
+  });
+
+  it("should provide lock button", () => {
+    const { getByText } = render(<Controls closed={true} locked={true} />);
+    expect(getByText(/lock gate/i)).toBeInTheDocument();
+  });
+  it("should switch lock gate to unlock button when locked", () => {
+    const { getByText } = render(<Controls closed={true} locked={true} />);
+    expect(getByText(/unlock gate/i)).toBeInTheDocument();
+  });
+
+  it("should provide close button", () => {
+    const { getByText } = render(<Controls closed={false} locked={false} />);
+    expect(getByText(/close gate/i)).toBeInTheDocument();
+  });
+
+  it("should switch close to open button when closed", () => {
+    const { getByText } = render(<Controls closed={true} locked={false} />);
+    expect(getByText(/open gate/i)).toBeInTheDocument();
   });
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import Dashboard from "./Dashboard";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
 
 describe("Dashboard", () => {
   it("should render without failing", () => {
