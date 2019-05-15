cd54d2d6e2c16af88d6ec8bc5469e00a8f1f0d93
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-15 16:44:56 -0400

    render successful test complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+// Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Controls from "./Controls";
+
+describe("Controls", () => {
+  it("should render without failing", () => {
+    render(<Controls />);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Display from "./Display";
+
+describe("Display", () => {
+  it("should render without failing", () => {
+    render(<Display />);
+  });
+});
